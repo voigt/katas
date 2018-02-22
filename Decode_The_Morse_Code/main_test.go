d2c4504f7d967e2dbeb1ce61caa4777bc25a4622
Christoph Voigt <voigt.christoph@gmail.com>
2018-02-22 15:57:12 +0100

    add unit tests for Decode_The_Morse_Code

---
+++ Decode_The_Morse_Code/main_test.go
@@ -0,0 +1,40 @@
+package main
+
+import "testing"
+
+var tables = []struct {
+	encodedStr string
+	expectStr  string
+}{
+	{".-", "a"},
+	{".", "e"},
+	{"..", "i"},
+	{". .", "ee"},
+	{".   .", "e e"},
+	{"...---...", ""},
+	{"... --- ...", "sos"},
+	{"...   ---   ...", "s o s"},
+
+	{" . ", "e"},
+	{"   .   . ", "e e"},
+	{"      ... --- ... -.-.--   - .... .   --.- ..- .. -.-. -.-   -... .-. --- .-- -.   ..-. --- -..-   .--- ..- -- .--. ...   --- ...- . .-.   - .... .   .-.. .- --.. -.--   -.. --- --. .-.-.-  ", "sos! the quick brown fox jumps over the lazy dog."},
+}
+
+// func TestDecodeMorse(t *testing.T) {
+
+// 	for _, table := range tables {
+// 		decodedStr := DecodeMorse(table.encodedStr)
+// 		if decodedStr != table.expectStr {
+// 			t.Errorf("Decoding of '%s' was incorrect got: %s, want: %s.", table.encodedStr, decodedStr, table.expectStr)
+// 		}
+// 	}
+// }
+
+func TestElegantDecodeMorse(t *testing.T) {
+	for _, table := range tables {
+		decodedStr := ElegantDecodeMorse(table.encodedStr)
+		if decodedStr != table.expectStr {
+			t.Errorf("Decoding of '%s' was incorrect got: %s, want: %s.", table.encodedStr, decodedStr, table.expectStr)
+		}
+	}
+}
